9eb5ce7fc687f3664cfb18404f2a3def09b426cb
weqopy <weqopy@gmail.com>
2020-12-12 08:26:28 +0800

    gitup

--- batch_pull.sh
+++ batch_pull.sh
@@ -1,16 +1,18 @@
 # git pull all repos
+#
+#cd ~/repos
+#for dir in $(ls)
+#do
+#    echo "===$dir==="
+#    if [[ "$skip_repos" =~ "$dir" ]]
+#    then
+#        echo "------skip---"
+#        continue
+#    fi
+#    cd ~/repos
+#    echo "------pulling---"
+#    cd $dir && git pull && cd ..
+#done
 
-cd ~/repos
-for dir in $(ls)
-do
-    echo "===$dir==="
-    if [[ "$skip_repos" =~ "$dir" ]]
-    then
-        echo "------skip---"
-        continue
-    fi
-    cd ~/repos
-    echo "------pulling---"
-    cd $dir && git pull && cd ..
-done
-
+# 使用 gitup 批量更新
+gitup
