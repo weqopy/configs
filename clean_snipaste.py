7e9413a543b41c6ae3b43910a44b736ed4512547
weqopy <weqopy@gmail.com>
2020-12-31 19:09:16 +0800

    update

--- clean_snipaste.py
+++ clean_snipaste.py
@@ -4,8 +4,8 @@ from shutil import move
 import zipfile
 
 user_name = os.environ.get("USER_NAME")
-folder = user_name + "Pictures/Snipaste/"
-trash = user_name + "trash/sps/"
+folder = user_name + "/Pictures/Snipaste/"
+trash = user_name + "/trash/sps/"
 
 now = datetime.today()
 i = 0
@@ -27,13 +27,13 @@ print(f"{len(items)-i} files stayed.")
 
 print("Alfred configs backup")
 
-backup_file = user_name + "Documents/Personal/alfred_configs.zip"
+backup_file = user_name + "/Documents/Personal/alfred_configs.zip"
 if os.path.exists(backup_file):
     print("old backup_file exists.")
     os.remove(backup_file)
 
 z = zipfile.ZipFile(backup_file, 'w', zipfile.ZIP_DEFLATED)
-configs = user_name + ".alfred/Alfred.alfredpreferences"
+configs = user_name + "/.alfred/Alfred.alfredpreferences"
 for dirpath, dirnames, filenames in os.walk(configs):
     for filename in filenames:
         z.write(os.path.join(dirpath, filename))
